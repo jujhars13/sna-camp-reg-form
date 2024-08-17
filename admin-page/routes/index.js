20f92d0ea6e9cb9aaa3c58be988b1a889896f36b
Jujhar Singh <jujhar@jujhar.com>
2024-08-17 16:16:02 +0100

    gets basic data, ready for part two

--- admin-page/routes/index.js
+++ admin-page/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+const createClient = require("@supabase/supabase-js").createClient;
+var express = require("express");
 var router = express.Router();
 
+const { SUPABASE_PRIVATE_KEY, SUPABASE_URL } = process.env;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async function (req, res, next) {
+  const supabase = createClient(SUPABASE_URL, SUPABASE_PRIVATE_KEY);
+
+  const { data, error } = await supabase.from("snacamp").select();
+  console.debug({ data, error });
+
+  res.render("index", { title: "Express", data });
 });
 
 module.exports = router;
