2ca266f4978758abb13d9e909b04f56aa374f1b3
Jujhar Singh <jujhar@jujhar.com>
2025-07-11 16:08:09 +0100

    "first tagged deploy"

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ npm test
 - [x] Write unit test
 - [x] Write db Schema for application
 - [ ] build and deploy application container
+- [x] sort out reverse proxy and DNS for deployments
 - [ ] Write kustomize manifest for app
 - [ ] Add ability to customise form for different events
 - [ ] Test end to end integration
---
+++ commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script to manage the semver incrementation and tagging of the git branch
+# relies on gum https://github.com/charmbracelet/gum being installed to make the input nice
+# `brew install gum`
+
+# (which gum > /dev/null) || echo >&2 "fum not installed, please install fum to use this script"; exit 12
+
+echo "Current version is $(node -p "require('./package.json').version")"
+TYPE=$(gum choose \
+    --header "What type of commit is this? Semver type?" \
+    --selected "patch" \
+     "major" "minor" "patch" )
+test -z "${TYPE}" && echo >&2 "Semver revision type not supplied" && exit 3
+
+SUMMARY=$(gum write --placeholder "Your commit message (CTRL+D to finish)")
+test -z "${SUMMARY}" && echo >&2 "\commit message not supplied" && exit 4
+
+git add .
+git commit -a --message \""$SUMMARY"\"
+
+# Commit these changes and tag the branch with the semver
+gum confirm "Commit changes and tag branch to deploy to production?" && \
+    npm version "${TYPE}" --message \""$SUMMARY"\" && \
+    git push && \
+    git push --tags
